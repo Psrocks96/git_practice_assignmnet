e349b2b30909a033f351ff9c2ec85633db6a6f1d
Psrocks96 <psbhardwaj62@gmail.com>
2022-11-06 23:48:42 +0530

    Optimised code

--- prime.js
+++ prime.js
@@ -1,9 +1,7 @@
 let num=11;
-for(let i=1;i<=num;i++){
-  let factor=0;
-  if(i%num==0){
+for(let i=2;i<=num**0.5;i++){
+  if(num%i==0){
     console.log("It is a prime number");
-  }else{
-    console.log("Not a prime number");
+    break;
   }
 }
